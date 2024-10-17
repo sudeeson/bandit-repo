2fe5e64350288a5a4bfdd96368d3f0f34b02011a
sudeeson <sudeeson.s@gmail.com>
2024-10-17 16:29:22 +0530

    added sample.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 
-# Function to reverse a string
-def reverse_string(s):
-    return s[::-1]
+# # Function to reverse a string
+# def reverse_string(s):
+#     return s[::-1]
 
-# Example usage
-original_string = "Python"
-reversed_string = reverse_string(original_string)
+# # Example usage
+# original_string = "Python"
+# reversed_string = reverse_string(original_string)
 
-print(f"The original string is: {original_string}")
-print(f"The reversed string is: {reversed_string}")
+# print(f"The original string is: {original_string}")
+# print(f"The reversed string is: {reversed_string}")
