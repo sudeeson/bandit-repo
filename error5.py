3b8e8607fef18a86c2c353384c3b89049310ba83
sudeeson <sudeeson.s@gmail.com>
2024-10-17 22:47:20 +0530

    modified error5.py

--- error5.py
+++ error5.py
@@ -1,6 +1,6 @@
 # This code will raise a ZeroDivisionError
 def divide_numbers(a, b):
-    return a / b
+return a / b
 
 # Calling the function with b = 0 will cause a division by zero error
 result = divide_numbers(10, 0)
